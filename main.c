6b62d68d12c248bc1e2611dc06e4ecdc8fa8dd07
Albert Chen <achen51@stuy.edu>
2025-01-06 15:35:47 -0500

    added file headers do something about input

---
+++ main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+ //What programs do we need on startup?
+
+
+//On opening a file will print out
+ void open(){
+
+
+
+ }
+
+//Appends s to the end of the file
+void append(char s[256],FILE *f){
+
+
+}
+
+//Upon typing the ""
+void exit(){}
+
+//Continuosuly takes the input from stdin and runs the corresponding function
+//Probably using execvp
+void input(){}
+
+
+//Using lseek and arrow keys to move places in the lines?
+//Start with left to right
+void movingPlaces(){}
