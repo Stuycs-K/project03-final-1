436afb76e1d1d6f6f9e1d5c10602044c349a72e3
AlbertCLaptopWSL <albclaptopwsl@ACLaptop.>
2025-01-13 00:22:37 -0500

    Worked on append file, need to change how it handles input while file's open vs when selecting file

--- DEVLOG.md
+++ DEVLOG.md
@@ -4,16 +4,23 @@ This document must be updated daily by EACH group member.
 
 ## Player 1
 
-### 2024-01-02 - Brief description
+### 1/6/2024- Added File Headers
 Expanded description including how much time was spent on task.
 
-### 2024-01-03 - Brief description
-Expanded description including how much time was spent on task.
+### 1/7/2024 - Understanding ncurses
+Watched tutorials and looked at webpages
 
-## Player 2
+### 1/11/2024 -
 
-### 2024-01-02 - Brief description
-Expanded description including how much time was spent on task.
+###  1/12/2024 - DId NOthing
+Went to Grandparent's place for Grandma's birthday then dragged to Costco, got home at 8PM meaning that file directory stuff got delayed
 
-### 2024-01-03 - Brief description
-Expanded description including how much time was spent on task.
+### 1/13/2024 - Append done, make error
+When testing got these errors, finished append code for when adding at the end of a line
+
+/usr/bin/ld: main.c:(.text+0x1d3): undefined reference to `LINES'
+/usr/bin/ld: main.c:(.text+0x20c): undefined reference to `stdscr'
+/usr/bin/ld: main.c:(.text+0x214): undefined reference to `wgetch'
+/usr/bin/ld: main.c:(.text+0x229): undefined reference to `printw'
+/usr/bin/ld: main.c:(.text+0x230): undefined reference to `stdscr'
+/usr/bin/ld: main.c:(.text+0x238): undefined reference to `wrefresh'
--- main.c
+++ main.c
@@ -12,17 +12,32 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
-#include <ncurses.h>
+  #include <ncurses.h>
 #include <sys/stat.h>
 #include <dirent.h>
  //What programs do we need on startup?
 
 
 //Parses args so that you can run it line should be from stdin
- void parse_args(char * line, char ** arg_ary){
+
+void parse_args( char * line, char ** arg_ary ){
+  //line should be a mutable string
+  //Arg_ary is long enough to store the tokens of the provided line
+  //1 space between every argument in string
+  //Last thing of arg_ary has to be NULL
+
+  // Parses the line maybe replace with \0
+  int count =0;
+  while(line){
+  arg_ary[count]=  strsep(&line," ");
+  count +=1;
+}
+//Sets the last argument of arg_ary to NULL
+arg_ary[count+1] = NULL;
+//PATH
 
 
- }
+}
 
 
   //Displays all .txt files in the directory runs out startup
@@ -51,7 +66,7 @@ while(readdir(dir)!=NULL){
 //Get file name from readdir's dirent
   stat(currentFile->d_name,stat_buffer);
 
-  printf("File Name: %s File Type: %s File Size: %d \n", currentFile->d_name,currentFile->d_type,stat_buffer->st_size);
+  printf("File Name: %s File Type: %d File Size: %d \n", currentFile->d_name,currentFile->d_type,stat_buffer->st_size);
 
   }
 }
@@ -65,6 +80,7 @@ int main (){
 
 //Opens a file and prints it out, will this allow it to edit?
  void opensss(char* f){
+   //Initial start up just prints everything
    int windowY = 0;
    int windowX = 0;
    char * buffer;
@@ -76,7 +92,6 @@ int main (){
 
  fopen(f, "w+");
 
-//Initial start up just prints everything
   while (buffer != "\0"){
 
 
@@ -118,14 +133,37 @@ int main (){
 
 
 //Appends s to the end of the file
-void append(char s[256],FILE *f){
+void append(char s,char *arg_ary){
+
+
+
+    int count =0;
+    while(arg_ary[count] != "\0"){
+    count +=1;
+  }
+  arg_ary[count] = s;
+  arg_ary[count+1]="\0";
+
 
 
 }
 
-//Upon typing the ""
-void exitFile(){}
+//Upon typing the "" automatically
+void exitFile(){
 
-//Continuosuly takes the input from stdin using fgets and runs the corresponding function
+
+
+}
+
+// takes the input from stdin using fgets and runs the corresponding function
 //Probably using execvp
-void inputFile(){}
+void inputFile(){
+
+  while(1){
+
+  }
+
+
+
+
+}
