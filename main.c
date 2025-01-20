9fc32ab517e765cc1fdbee172fa5de94f9f96b1d
AlbertCLaptopWSL <albclaptopwsl@ACLaptop.>
2025-01-19 19:02:19 -0500

    fixed issue with file displaying on startup

--- main.c
+++ main.c
@@ -21,6 +21,78 @@
  //What programs do we need on startup?
 
 
+ // Done for now
+   //Displays all .txt files in the directory runs out startup
+   void display(){
+     //Setup
+     char s[256];
+     char* directory = getcwd(s,256);
+     DIR *dir =  opendir(directory);
+     struct dirent * currentFile;
+
+     struct stat * stat_buffer;
+     //Maybe causing a seg fault maybe not
+      stat_buffer = malloc(sizeof(struct stat)*1);
+      //This part causes segfault
+      //First opens directory then loops through all the files assining values then prints out
+      while((currentFile = readdir(dir)) != NULL){
+
+      //Get file name from readdir's dirent
+        stat(currentFile->d_name,stat_buffer);
+        printf(" File Name: %s File Type: %d File Size: %ld \n", currentFile->d_name,currentFile->d_type,stat_buffer->st_size);
+
+        }
+
+
+ }
+
+ //Upon typing the "" automatically saves changes made from window to file and closes the file
+ void exitFile(FILE*f,int fd){
+
+
+ fclose(f);
+ endwin();
+ display();
+ //close(f);
+ }
+
+
+
+
+ //Done
+ void offsetAdd(char input, char* buffer, int currentPos){
+ int len = strlen(buffer);
+
+   buffer[len+2] = '\0';
+   for (int i = len; i>= currentPos ;i--){
+     printf("%d i: %d \n" ,len-currentPos -i,i);
+     printf("Current pair; One in front: %c One that replaces it: %c \n",buffer[(len-currentPos) -i],buffer[len-(currentPos) -(i+1)]);
+     //Should go backwards replacing current with the one before it
+     buffer[i+1] = buffer[i];
+
+   }
+ buffer[currentPos] = input;
+ buffer[len+1]='\0';
+
+ }
+
+
+
+
+ //Appends s to the end of the file NOT TO THE WINDOW
+ //Need to change end
+ //Done
+ void append(char s,char x[256]){
+   int count =0;
+   while(x[count] != '\0'){
+     count +=1;
+   }
+   x[count] = s;
+   x[count+1]='\0';
+ }
+
+
+
  //Opens a file and prints it out, will this allow it to edit?
   void opensss(int fileDescriptor, char*fileString){
     //Initial start up just prints everything
@@ -35,11 +107,10 @@
     buffer = (char*) malloc(sizeof(char)* LINES);
     //Buffer for reading from input
     inputBuffer = (char*) malloc(sizeof(char) *LINES);
-  FILE * file=  fopen(fileString, "w+");
-
-
-  while (fgets(buffer, LINES, file)) {
+  FILE * file=  fopen(fileString, "r+");
+  while (fgets(buffer, LINES, file) !=NULL) {
        printw("%s", buffer);
+       refresh();
    }
 
 
@@ -60,6 +131,10 @@
       if(input ==KEY_UP){
         move(getcury(stdscr)-1,getcurx(stdscr));
       }
+      else if(input ==27){ //Exits when you hit esc
+        break;
+        exitFile(file,fileDescriptor);
+      }
       else if(input ==KEY_DOWN){
         move(getcury(stdscr)+1,getcurx(stdscr));
       }
@@ -70,16 +145,15 @@
         move(getcury(stdscr),getcurx(stdscr)+1);
       }
       else{
-        printw("%c",input);
+      //  printw("%c",input);
         //Using fputc here or offset add/append?
 
  // put that function here
         //Should write in the file using buffer
-
    }
-     refresh();
-    }
 
+    }
+refresh();
 
   }
 
@@ -108,30 +182,6 @@ arg_ary[count+1] = NULL;
 
 }
 
-// Done for now
-  //Displays all .txt files in the directory runs out startup
-  void display(){
-    //Setup
-    char s[256];
-    char* directory = getcwd(s,256);
-    DIR *dir =  opendir(directory);
-    struct dirent * currentFile;
-
-    struct stat * stat_buffer;
-    //Maybe causing a seg fault maybe not
-     stat_buffer = malloc(sizeof(struct stat)*1);
-     //This part causes segfault
-     //First opens directory then loops through all the files assining values then prints out
-     while((currentFile = readdir(dir)) != NULL){
-
-     //Get file name from readdir's dirent
-       stat(currentFile->d_name,stat_buffer);
-       printf(" File Name: %s File Type: %d File Size: %ld \n", currentFile->d_name,currentFile->d_type,stat_buffer->st_size);
-
-       }
-
-
-}
 
 int main (){
   //Will be reponsibole for printing and editing and saving to maybe?
@@ -153,7 +203,7 @@ int main (){
      opensss(fd,argsArray[1]);
    }
    else{ //if it dne then creates a new file
-     int fd = open(argsArray[1], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+     int fd = open(argsArray[1], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR, 0644);
     opensss(fd, argsArray[1]);
    }
 
@@ -167,47 +217,3 @@ int main (){
 }
 
 //Make size of file buffer max like 8k
-
-
-
-//Done
-void offsetAdd(char input, char* buffer, int currentPos){
-int len = strlen(buffer);
-
-  buffer[len+2] = '\0';
-  for (int i = len; i>= currentPos ;i--){
-    printf("%d i: %d \n" ,len-currentPos -i,i);
-    printf("Current pair; One in front: %c One that replaces it: %c \n",buffer[(len-currentPos) -i],buffer[len-(currentPos) -(i+1)]);
-    //Should go backwards replacing current with the one before it
-    buffer[i+1] = buffer[i];
-
-  }
-buffer[currentPos] = input;
-buffer[len+1]='\0';
-
-}
-
-
-
-
-//Appends s to the end of the file NOT TO THE WINDOW
-//Need to change end
-//Done
-void append(char s,char x[256]){
-  int count =0;
-  while(x[count] != '\0'){
-    count +=1;
-  }
-  x[count] = s;
-  x[count+1]='\0';
-}
-
-//Upon typing the "" automatically saves changes made from window to file and closes the file
-void exitFile(char*f,int fd){
-
-
-close(fd);
-endwin();
-display();
-//close(f);
-}
